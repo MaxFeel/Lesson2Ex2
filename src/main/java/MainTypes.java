621a603f1e6faae3ca5038269b9054bd07314b36
Maxim <mr.maks.filippov.94@mail.ru>
2021-03-03 20:19:20 +0300

    Initial commit

---
+++ src/main/java/MainTypes.java
@@ -0,0 +1,14 @@
+public class MainTypes {
+    public static void main(String[] args) {
+        byte b = 127;
+        short sh = 32000;
+        int i = 199999999;
+        long l = 123456789L;
+        float f = 120.05f;
+        double d = 1353.097;
+        char c = 'd';
+        String s = "Hello world";
+        boolean bull = true;
+
+    }
+}
